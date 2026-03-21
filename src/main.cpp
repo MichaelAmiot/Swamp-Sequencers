aade35370d20e39c1f3658060cf35aeb1833d4ee
Jaefae <Jaefae@icloud.com>
2026-03-21 14:21:07 -0400

    Add memory mapping for file IO and minimal example

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
-
-int main(){
-
-
-
-
-
-
-
-    return 0;
+#include "SwampSeqLib/genome_mapper.h"
+#include <iostream>
+int main(int argc, char *argv[]) {
+  GenomeMapper mapped(argv[1]);
+  const char *data = mapped.data();
+  if (!mapped.isValid()) {
+    std::cerr << "Could not open file: " << argv[1] << "\n";
+  }
+  std::cout << "Size of data: " << mapped.size() << "\n";
+  std::cout << "First 1000 characters:\n";
+  for (int i = 0; i < 1000; i++) {
+    std::cout << data[i];
+  }
+  return 0;
 }
